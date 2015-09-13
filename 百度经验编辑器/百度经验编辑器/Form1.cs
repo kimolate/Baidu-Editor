8422824fbbf024dfd1d6a1476ddb95079e83d1c8
kimolate <kimolatelei@outlook.com>
2015-09-13 19:54:58 +0800

    V0.1
    实现单击步骤框,图片会跟着改变。
    实现生成word,只能向word添加文字,图片不能。

---
+++ 百度经验编辑器/百度经验编辑器/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using 百度经验编辑器.MainClass;
+
+namespace 百度经验编辑器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public 百度经验编辑器.MainClass.PublicClass mainclass = new 百度经验编辑器.MainClass.PublicClass();
+        string strContent;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "D:\\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fullpath = openFileDialog1.FileName;
+                PublicClass.photopath = fullpath.Substring(0, fullpath.LastIndexOf('\\')+1) ;
+                mainclass.FirstPhoto(fullpath, pictureBox1);
+                textBox1.Focus();
+                
+            }
+          
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            PublicClass.tag = textBox.Tag.ToString();
+            mainclass.AutoChangePhoto(pictureBox1);
+        }
+        public string getText()
+        {
+            string strText = null;
+            if (textBox1.Text != "")
+                strText = textBox1.Text + '\n';
+            if (textBox2.Text != "")
+                strText = strText + textBox2.Text + '\n';
+            if (textBox3.Text != "")
+                strText = strText + textBox3.Text + '\n';
+            if (textBox4.Text != "")
+                strText = strText + textBox4.Text + '\n';
+            if (textBox5.Text != "")
+                strText = strText + textBox5.Text + '\n';
+            if (textBox6.Text != "")
+                strText = strText + textBox6.Text + '\n';
+            if (textBox7.Text != "")
+                strText = strText + textBox7.Text + '\n';
+            if (textBox8.Text != "")
+                strText = strText + textBox8.Text + '\n';
+            if (textBox9.Text != "")
+                strText = strText + textBox9.Text + '\n';
+            return strText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mainclass.GetWord(getText());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+       
+
+       
+
+       
+    }
+}
---
+++ 百度经验编辑器/百度经验编辑器/MainClass/PublicClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Reflection;
+
+namespace 百度经验编辑器.MainClass
+{
+   public class PublicClass
+    {
+
+       public static string tag=null ;
+       public static string photopath=null;
+        
+
+
+        public void AutoChangePhoto(PictureBox pic)
+        {
+            string picturepath = photopath + tag+".png";
+            Bitmap photo = new Bitmap(picturepath);
+            pic.Image = (Image)photo;
+            
+            
+        }
+        public void GetWord(string strContent)
+        {
+            object path;//文件路径
+           
+            MSWord.Application wordApp;//Word应用程序变量
+            MSWord.Document wordDoc;//Word文档变量
+            path = "d:\\myWord.doc";//保存为Word2003文档
+            // path = "d:\\myWord.doc";//保存为Word2007文档
+            wordApp = new MSWord.ApplicationClass();//初始化
+            if (File.Exists((string)path))
+            {
+                File.Delete((string)path);
+            }
+            Object Nothing = Missing.Value;
+            wordDoc = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+
+            wordApp.Selection.ParagraphFormat.LineSpacing = 35f;//设置文档的行间距
+            //写入普通文本
+            wordApp.Selection.ParagraphFormat.FirstLineIndent = 30;//首行缩进的长度
+            
+            wordDoc.Paragraphs.Last.Range.Text = strContent;
+            object format = MSWord.WdSaveFormat.wdFormatDocument;
+            wordDoc.SaveAs(ref path, ref format, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            wordDoc.Close(ref Nothing, ref Nothing, ref Nothing);
+            wordApp.Quit(ref Nothing, ref Nothing, ref Nothing);
+           
+
+        }
+        public void FirstPhoto(string path, PictureBox pic)
+        {
+            Bitmap firstphoto = new Bitmap(path);
+            pic.Image = (Image)firstphoto;
+        }
+            
+       
+
+
+    }
+}
